8c3689d7b70c764d8c928c1059d763929ec86838
MiraSkold <mira.skold@elev.ga.lbs.se>
2025-01-09 15:16:40 +0100

    Recoil och byta vapen

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
@@ -6,10 +7,31 @@ public class Gun : MonoBehaviour
     public float range = 100f;
     public Camera fpsCam;
     public ParticleSystem MuzzleFlash;
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
 
     // Update is called once per frame
+    void Start()
+    {
+        if(currentAmmo == -1)
+        currentAmmo = maxAmmo;
+    }
+    void OnEnable()
+    {
+        isReloading = false;
+
+    }
     void Update()
     {
+        if (isReloading)
+            return;
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
@@ -17,9 +39,20 @@ public class Gun : MonoBehaviour
         }
 
     }
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading");
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
     void Shoot ()
     {
         MuzzleFlash.Play();
+
+        currentAmmo--;
+
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
